d7ad3a550200af86f05e38c713440cdf0b2c72c5
Saurabh Kumar <saurabhkumar67172@gmail.com>
2026-05-03 22:27:42 +0530

    fix: cors for vercel

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://team-task-manager-henna.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 connectDB();
