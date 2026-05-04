cf6935b165a6a904199c9e20643c0ab8ba80ccc7
Saurabh Kumar <saurabhkumar67172@gmail.com>
2026-05-04 11:10:18 +0530

    fix port issue

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
